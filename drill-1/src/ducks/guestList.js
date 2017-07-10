7e5ed26b43dd1e50dd08a582db35e79efa722738
Sean <sevensidedsilver@gmail.com>
2017-07-10 15:48:20 -0600

    basic setup drill 1

---
+++ drill-1/src/ducks/guestList.js
@@ -0,0 +1,10 @@
+// since this is redux, no need to import react
+
+
+const initialState={}
+
+// reducers take in state and action
+export default function reducer(state = initialState, action) {
+  return state
+
+}
---
+++ drill-1/src/store.js
@@ -0,0 +1,4 @@
+import {createStore} from 'redux'
+import reducer from './ducks/guestList'
+
+export default createStore(reducer);
